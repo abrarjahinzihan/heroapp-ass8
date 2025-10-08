8761f1f5910ae6242f717ed5132bd0f71ff4dfed
Abrar Jahin Zihan <abrarjahinkhanzihan@gmail.com>
2025-10-08 20:53:55 +0600

    main layout added

---
+++ src/Layout/MainLayOut.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Outlet } from "react-router";
+import Footer from "../components/Footer";
+import Banner from "../components/Banner";
+import { ToastContainer } from "react-toastify";
+
+const MainLayOut = () => {
+    return (
+        <div className=" flex flex-col min-h-screen bg-[#f5f7f9]">
+        <Navbar></Navbar>
+
+        <div className="flex-1">
+            <Outlet></Outlet>
+        </div>
+        <Footer></Footer>
+        <ToastContainer />
+        </div>
+    );
+};
+
+export default MainLayOut;
